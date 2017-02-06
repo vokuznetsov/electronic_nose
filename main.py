bbb7cb594064724e9823f6ab35b67cfa90a4b680
Vladimir Kuznetsov <vokuznecov@gmail.com>
2017-02-06 23:25:52 +0300

    small-changes

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from scipy.spatial import distance
 # vim: set fileencoding=utf-8 :
 
 START_COLUMN = 1
-NUMBER_OF_FIRST_MEASUREMENT = 16
+NUMBER_OF_FIRST_MEASUREMENT = 11
 list_of_files = ['belkozin_21_07_14_17_48.xls', 'vodka_proba_1_21_05_14_18_35.XLS', 'voda_proba_1_20_05_14_19_33.XLS',
                  'beef_50_23_05_14_14_42.xls', 'cheeken_50_23_05_14_14_12.XLS', 'aceton_14_05_14_13_20.XLS',
                  'izopropanol_14_05_14_16_22.XLS', 'ethylacetate_16_05_14_15_49.XLS', 'uksus_2_09_14_14_13.xls',
                  'voda_distil_17_09_14_15_20.xls', 'etanol_3_07_14_17_25.xls', 'aceton_1_07_14_15_45.XLS',
-                 'etanol_4_07_14_16_40.XLS', 'suharik_29_06_09_17_33.xls', 'suharik_29_06_09_17_36.XLS']
+                 'etanol_4_07_14_16_40.XLS', 'suharik_29_06_09_17_33.xls', 'suharik_29_06_09_17_36.XLS',
+                 'aceton_14_05_14_13_51.XLS']
 
 
 def parsing(path_to_file):
@@ -43,13 +44,14 @@ def get_information_from_measurment():
         print path_to_file + ' - ' + str(count)
         arr = parsing('./resource/' + path_to_file)
 
-        first_max = np.array(arr[0:NUMBER_OF_FIRST_MEASUREMENT, 0]).max()
-        last_mean = abs(np.array(arr[NUMBER_OF_FIRST_MEASUREMENT:arr.shape[0], 0]).mean())
-        ratio = abs(first_max / last_mean)
-
-        vector.append(first_max)
-        vector.append(last_mean)
-        vector.append(ratio)
+        sensors = [0, 1, 2, 3, 4, 5, 6, 7]
+        for col in sensors:
+            first_max = np.array(arr[0:NUMBER_OF_FIRST_MEASUREMENT, col]).max()
+            last_mean = np.array(arr[NUMBER_OF_FIRST_MEASUREMENT:arr.shape[0], col]).mean()
+            # ratio = abs(first_max / last_mean)
+            vector.append(first_max)
+            vector.append(last_mean)
+            # vector.append(ratio)
         if result.shape[0] == 0:
             result = np.append(result, vector)
         else:
@@ -58,7 +60,7 @@ def get_information_from_measurment():
     return result.transpose()
 
 
-# method computes closeness of dest vector with src_1 and src_2 vectors based on euclidean measurment
+# method computes closeness of dest vector with src_1 and src_2 vectors based on euclidean measurement
 def closeness(src_1, src_2, dest):
     dist_1 = distance.euclidean(src_1, dest)
     print str(dist_1) + ' is closeness between first measurement and destination vector'
@@ -82,7 +84,7 @@ def closeness_all(index):
 result = get_information_from_measurment()
 print '\n'
 
-closeness(result[:, 4], result[:, 1], result[:, 10])
+closeness(result[:, 0], result[:, 1], result[:, 12])
 
 print '\n'
-closeness_all(4)
+closeness_all(2)
